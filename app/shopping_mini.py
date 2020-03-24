94d61be5a776a32768844246f580975be23867d3
Calvin <cod11@georgetown.edu>
2020-03-24 14:40:59 -0400

    refactored code

--- app/shopping_mini.py
+++ app/shopping_mini.py
@@ -1,8 +1,7 @@
 
 from datetime import datetime
 import os
-
-checkout_at = datetime.now().strftime("%M/%d/%Y %I:%m %p")
+import sys
 
 selected_products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
@@ -12,39 +11,30 @@ selected_products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
 ] # FYI: for the purposes of this exercise, you won't need to modify this list at all
 
-now = datetime.now()
+#RECEIPT FUNCTION
+def printReceipt(outputFunction):
+    outputFunction("---------" + "\n")
+    outputFunction("CHECKOUT AT: " + str(now.strftime("%Y-%M-%d %H:%m:%S")) + "\n")
+    outputFunction("---------" + "\n")
+    for p in selected_products:
+        outputFunction("SELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]) + "\n")
+    outputFunction("---------" + "\n")
+    outputFunction(f"SUBTOTAL: {subtotal:,.2f}" + "\n")
+    outputFunction(f"TAX: {(subtotal * 0.0875):.2f}" + "\n")
+    outputFunction(f"TOTAL: {((subtotal * 0.0875) + subtotal):.2f}" + "\n")
+    outputFunction("---------" + "\n")
+    outputFunction("THANK YOU! PLEASE COME AGAIN SOON!" + "\n")
+    outputFunction("---------" + "\n")
 
+now = datetime.now()
 subtotal = sum([p["price"] for p in selected_products])
 
 # PRINT RECEIPT
-
-print("---------")
-print("CHECKOUT AT: " + str(now.strftime("%Y-%M-%d %H:%m:%S")))
-print("---------")
-for p in selected_products:
-    print("SELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]))
-print("---------")
-print(f"SUBTOTAL: {subtotal:,.2f}")
-print(f"TAX: {(subtotal * 0.0875):.2f}")
-print(f"TOTAL: {((subtotal * 0.0875) + subtotal):.2f}")
-print("---------")
-print("THANK YOU! PLEASE COME AGAIN SOON!")
-print("---------")
+printReceipt(sys.stdout.write)
 
 # WRITE RECEIPT TO FILE
-
 file_name = os.path.join(os.path.dirname(__file__), "..", "receipts", f"{now.strftime('%Y-%M-%d-%H-%m-%S')}.txt")
 with open(file_name, 'w') as f:
-    f.write("------------------------------------------")
-    for p in selected_products:
-        f.write("\nSELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]))
-
-    f.write("---------")
-    f.write(f"SUBTOTAL: {subtotal:,.2f}")
-    f.write(f"TAX: {(subtotal * 0.1):.2f}")
-    f.write(f"TOTAL: {((subtotal * 0.1) + subtotal):.2f}")
-    f.write("---------")
-    f.write("THANK YOU! PLEASE COME AGAIN SOON!")
-    f.write("---------")
+    printReceipt(f.write)
 
 # TODO: SEND RECEIPT VIA EMAIL
